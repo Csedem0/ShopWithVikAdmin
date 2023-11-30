5efd8bb150144725b01cff8d3a0077f7630c9b24
Csedem0 <emmasobula@gmail.com>
2023-11-30 04:08:48 -0800

    token fixed

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL = "https://shopwithvikbackend.onrender.com/api/";
 
 const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NjJmZTE3ZGE5OTRiMTNiOGQ4ZDkyZCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTcwMDk5MzE2MywiZXhwIjoxNzAxMzM4NzYzfQ.Ne9VC1_nMnMKa5UlNQZkjQHqUtkyLyPjG_D4WY-SBuM";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1Njg3OTllNjdiM2NiOTJjODc0ZWY2MiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTcwMTM0NTgyMSwiZXhwIjoxNzAxNjkxNDIxfQ.t7uC8R7t_GVbTgZbHXI80vk_2wkSihKB_4iSn4E7qgI";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
