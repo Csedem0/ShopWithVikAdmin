68c2694e5241f7743941f3f4d71ef71ecc3dd4c0
Csedem0 <emmasobula@gmail.com>
2023-12-17 23:41:10 -0800

    token updated

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL = "https://shopwithvikbackend.onrender.com/api/";
 
 const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZmRiMDY3YWYyNTYzMTdkYzc4ZmRiMSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTcwMjIxMzExMiwiZXhwIjoxNzAyNTU4NzEyfQ.TvNqIjyZV9HE8nDo_KGI0NkrQBtZOVNRAyxRdeQjKd4";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZmRiMDY3YWYyNTYzMTdkYzc4ZmRiMSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTcwMjg4NTA4MSwiZXhwIjoxNzAzMjMwNjgxfQ.jo0nroR0PtBaPH87kT_RsbxkOMLqJf2fJTkWxgodD1A";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
